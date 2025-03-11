040d710b83e91987d70da46f6e5633e43fda1356
YeeYeeYES <yousefmoustafah@gmail.com>
2025-03-11 15:49:02 -0700

    Very cool new srtuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "TeleopLite", group = "Pushbot")
 
 public class TeleopTest extends OpMode {
-    public static int bumper_sens;
+    public static float bumper_sens;
     public static double gyro_pos;
     public static double intake_pos;
 
@@ -15,7 +15,7 @@ public class TeleopTest extends OpMode {
     @Override
     public void init() {
         System.out.println("Bismillah");
-        bumper_sens = 10;
+        bumper_sens = 0.005f;
         gyro_pos = 0;
         intake_pos = 0;
         robot.init(hardwareMap);
@@ -40,24 +40,22 @@ public class TeleopTest extends OpMode {
         double left_power = (gamepad1.right_stick_y + gamepad1.right_stick_x) * 2.0f;
         double right_power = (gamepad1.right_stick_y - gamepad1.right_stick_x) * 2.0f;
 
-        intake_pos += (gamepad1.right_trigger - gamepad1.left_trigger) * 2.0f;
+        intake_pos += (gamepad1.right_trigger - gamepad1.left_trigger) * 0.005f;
 
         if(gamepad1.right_bumper) {
-            System.out.println("Gyrating right Insha'allah");
             gyro_pos += bumper_sens;
         } else if(gamepad1.left_bumper) {
-            System.out.println("Gyrating left Insha'allah");
             gyro_pos -= bumper_sens;
         }
 
         //Arm and wheels
         robot.arm.setPower(arm_power);
-        robot.left.setPower(left_power);
-        robot.right.setPower(-right_power);
+        robot.left.setPower(-left_power);
+        robot.right.setPower(right_power);
 
         //Gyro Servo
         robot.intake.setPosition(intake_pos);
-        robot.gyro.setPosition(intake_pos);
+        robot.gyro.setPosition(gyro_pos);
     }
 
     // Code to run ONCE after the driver hits STOP
