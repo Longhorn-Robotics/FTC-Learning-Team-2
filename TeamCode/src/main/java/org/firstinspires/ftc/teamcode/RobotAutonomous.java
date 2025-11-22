d4954e4c4741e2b29b7538c732ca369d7e9d4896
Angad-D <angad.sub@gmail.com>
2025-11-22 09:04:03 -0800

    Adjustmnets to driving values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutonomous.java
@@ -62,11 +62,13 @@ public class RobotAutonomous extends LinearOpMode {
     //  Set the GAIN constants to controlxx the relationship between the measured position error, and how much power is
     //  applied to the drive motors to correct the error.
     //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
-    final double SPEED_GAIN =   0.02 ;   //  Speed Control "Gain". e.g. Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
-    final double TURN_GAIN  =   0.01 ;   //  Turn Control "Gain".  e.g. Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+    //speed gain was 0.02
+    final double SPEED_GAIN =   0.2 ;   //  Speed Control "Gain". e.g. Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    //was 0.01
+    final double TURN_GAIN  =   0.05 ;   //  Turn Control "Gain".  e.g. Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
 
 
-    final double MAX_AUTO_SPEED = 0.4;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_SPEED = 0.3;   //  Clip the approach speed to this max value (adjust for your robot)
     final double MAX_AUTO_TURN  = 1;  //  Clip the turn speed to this max value (adjust for your robot)
 
 
@@ -240,7 +242,7 @@ public class RobotAutonomous extends LinearOpMode {
 
         waitForStart();
         robot.runIntake();
-        while (true) {
+        while (opModeIsActive()) {
             String state = driveToClosestBall(-1);
         }
 
